a5a0f20820fadc93e378455ac5f95379f4d51a6d
noob-master-jpb <130906702+noob-master-jpb@users.noreply.github.com>
2026-04-25 15:01:33 +0530

    feat: add model download script, model manifest, and inference test script

---
+++ test2.py
@@ -0,0 +1,23 @@
+import requests
+url = "https://inference.do-ai.run/v1/chat/completions"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer sk-do-LJwtalqUMyKXNLkGsKDyoAE9C9LxfVPQ_y_745zbiG32nFb3pN88x4ACnj"
+}
+data = {
+    "model": "kimi-k2.5",
+    "messages": [
+        {   
+            "role": "user",
+            "content": "Tell me some fun facts about octopuses"
+        }
+    ],
+    "max_tokens": None,
+    "reasoning": {
+        "effort": "low",
+      "max_tokens": 1024
+    },
+}
+from pprint import pprint
+response = requests.post(url, headers=headers, json=data)
+pprint(response.json())
