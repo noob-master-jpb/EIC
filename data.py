453e0d790af4f6fc1019bdd967d955c8ea1fca16
noob-master-jpb <130906702+noob-master-jpb@users.noreply.github.com>
2026-04-26 05:19:16 +0530

    feat: implement dataset processing pipeline and integrate with training script

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import datasets
+
+
 def load_dataset(path:str):
     """
     Supported Formats:
@@ -21,7 +23,15 @@ def load_dataset(path:str):
         raise ValueError("Unsupported file format")
     return df
 
-def format_dataset(df:pd.DataFrame,user_prompt:str,agent_response:str,dataset:bool=True):
+def format_dataset(df:pd.DataFrame,user_prompt:str,agent_response:str,as_hf_dataset:bool=True):
+    """
+    Args:
+        df (pd.DataFrame): DataFrame containing the dataset
+        user_prompt (str): Column name for the user prompt
+        agent_response (str): Column name for the agent response
+        as_hf_dataset (bool): Whether to return a Hugging Face Dataset    
+    """
+
     messages = []
     for _, val in df.iterrows():
         
@@ -34,10 +44,31 @@ def format_dataset(df:pd.DataFrame,user_prompt:str,agent_response:str,dataset:bo
         )
         messages.append(conv)
     dataset = {'messages' : messages}
-    if dataset:
+    if as_hf_dataset:
         return datasets.Dataset.from_dict(dataset)
     return dataset
-    
-def load_format_dataset(path:str,user_prompt:str,agent_response:str,dataset:bool=True):
-    return format_dataset(load_dataset(path),user_prompt,agent_response,dataset)
 
+def process_dataset(path:str,user_prompt:str,agent_response:str,processor=None,num_proc=4):
+    """
+    Args:
+        path (str): Path to the dataset file
+        user_prompt (str): Column name for the user prompt
+        agent_response (str): Column name for the agent response
+        processor (AutoProcessor): Processor to use for formatting
+
+    Returns:
+        datasets.Dataset: Processed dataset
+
+    Note:
+        It will always return hf dataset
+    """
+    if processor is None:
+        raise ValueError("Processor is required")
+    df = load_dataset(path)
+    dataset=format_dataset(df,user_prompt,agent_response,as_hf_dataset=True)
+    final_ds = dataset.map(
+        lambda x: {"text": processor.apply_chat_template(x["messages"], tokenize=False)},
+        remove_columns=["messages"],
+        num_proc=num_proc
+    )
+    return final_ds
